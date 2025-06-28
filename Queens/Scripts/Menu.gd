80220e552586597da2954507b1d7f02199d0ad1a
Miguel <miguelgonzaloledesma@gmail.com>
2025-06-28 11:30:38 -0300

    Se agrega menu principal

--- Queens/Scripts/Menu.gd
+++ Queens/Scripts/Menu.gd
@@ -1,14 +1,21 @@
 extends Control
 
+@onready var start_audio: AudioStreamPlayer = $MarginContainer/Buttons/Start/AudioStreamPlayer
+@onready var credits_audio: AudioStreamPlayer = $MarginContainer/Buttons/Credits/AudioStreamPlayer
+@onready var exit_audio: AudioStreamPlayer = $MarginContainer/Buttons/Exit/AudioStreamPlayer
+
 var main_scene = "res://Scenes/Main.tscn"
 
 func _on_start_pressed() -> void:
+	start_audio.playing = true
 	get_tree().change_scene_to_file(main_scene)
 
 
 func _on_credits_pressed() -> void:
+	credits_audio.playing = true
 	pass # Replace with function body.
 
 
 func _on_exit_pressed() -> void:
+	exit_audio.playing = true
 	get_tree().quit()
